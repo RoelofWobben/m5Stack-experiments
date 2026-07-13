ea7a1ee05e5c3509815083e288c41532c44db573
Roelof Wobben <r.wobben@home.nl>
2026-07-13 14:00:38 +0200

    2 panels working

--- 04-panels/panel/panel.ino
+++ 04-panels/panel/panel.ino
@@ -5,23 +5,18 @@ struct RectButton {
   const char* label;
 };
 
-struct Panel{    
+struct Panel {
   int x, y, w, h;
-  const char* label; 
+  const char* label;
 };
 
 // drie panelen
-Panel lightPanel = {10,10,300,100, "Light"};
-Panel pompPanel = {10,130,300,100, "Pomp"};
-
-
-
-RectButton onButton = { panel.x + 20, panel.y + 50, 120, 40, "ON" };
-RectButton offButton = { panel.x + 160, panel.y + 50, 120, 40, "OFF" };
+Panel lightPanel = { 10, 10, 300, 100, "Light" };
+Panel pompPanel = { 10, 130, 300, 100, "Pomp" };
 
 // Licht status
-bool lightStatus = false; 
-bool pompStatus = false; 
+bool lightStatus = false;
+bool pompStatus = false;
 
 uint16_t panelColor = 0x18E3;
 uint16_t grey = 0x39C7;
@@ -37,15 +32,15 @@ void drawPanel(const Panel& panel) {
 
 void drawPanels() {
   drawPanel(lightPanel);
-  drawPanel(pompPanel); 
+  drawPanel(pompPanel);
 }
 
 void drawSingleButton(const RectButton& button, bool isActive) {
   M5.Display.fillRect(button.x - 2, button.y - 2, button.w - 4, button.h + 4, panelColor);
 
-  uint16_t color = isActive ? GREEN: grey;
+  uint16_t color = isActive ? GREEN : grey;
   M5.Display.fillRoundRect(button.x, button.y, button.w, button.h, 10, color);
-  
+
 
   M5.Display.setTextColor(WHITE, color);
   M5.Display.setTextSize(2);
@@ -54,9 +49,12 @@ void drawSingleButton(const RectButton& button, bool isActive) {
 }
 
 
-void drawButtons() {
-  //drawSingleButton(onButton, lightStatus == true);
-  //drawSingleButton(offButton, lightStatus == false);
+void drawButtons(const Panel& panel, bool status) {
+  RectButton onButton(panel.x + 20, panel.y + 50, 120, 40, "ON");
+  RectButton offButton(panel.x + 160, panel.y + 50, 120, 40, "OFF");
+
+  drawSingleButton(onButton, status);
+  drawSingleButton(offButton, !status);
 }
 
 bool isButtonTouched(const RectButton& button) {
@@ -69,26 +67,42 @@ bool isButtonTouched(const RectButton& button) {
   return (detail.x >= button.x && detail.x <= button.x + button.w && detail.y >= button.y && detail.y <= button.y + button.h);
 }
 
+RectButton getOnButton(const Panel& panel) {
+  return { panel.x + 20, panel.y + 50, 120, 40, "ON" };
+}
+
+RectButton getOffButton(const Panel& panel) {
+  return { panel.x + 160, panel.y + 50, 120, 40, "OFF" };
+}
+
+void handlePanelTouch(const Panel& panel, bool& status) {
+
+  if (!status && isButtonTouched(getOnButton(panel))) {
+    status = true;
+    drawButtons(panel, status);
+  }
+
+  if (status && isButtonTouched(getOffButton(panel))) {
+    status = false;
+    drawButtons(panel, status);
+  }
+}
+
 void setup() {
   auto cfg = M5.config();
   M5.begin(cfg);
   M5.Display.fillScreen(BLACK);
-  drawPanels();
-  //drawButtons();
+
+  drawPanel(lightPanel);
+  drawButtons(lightPanel, lightStatus);
+
+  drawPanel(pompPanel);
+  drawButtons(pompPanel, pompStatus);
 }
 
 void loop() {
   M5.update();
 
-  /*
-  if (lightStatus != true && isButtonTouched(onButton)) {
-    lightStatus = true;
-    drawButtons();
-  }
-
-  if (lightStatus != false && isButtonTouched(offButton)) {
-    lightStatus = false;
-    drawButtons();
-  }
-  */
+  handlePanelTouch(lightPanel, lightStatus);
+  handlePanelTouch(pompPanel, pompStatus);
 }
